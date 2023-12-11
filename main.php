b9c58e951af4e27f68a492d7111dbfe33a674151
NirajSingh2000 <snirajsingh678@gmail.com>
2023-12-11 10:50:03 +0530

    Youtube API V3

---
+++ main.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<p>Replace your YouTube URL in this box</p>
+<div id="divID" class="container-class">
+    <form method="post" action="details.php">
+        <input class="input-class" type="text" name="url" placeholder="Enter your URL" value="https://www.youtube.com/watch?v=aLONwnlBB-g">
+        <br />
+        <br />
+        <input class="submit-class" type="submit" name="submit" value="Submit URL">
+    </form>
+</div>
+</body>
+</html>
+
